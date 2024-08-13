4869b45f8a09ce9c49bf9024842f8266db2e23e4
jhdodev <jhdo.dev@gmail.com>
2024-08-13 14:58:04 +0900

    feat: 채팅방 목록 추가, 공통사용 Color 추가및수정, AppBar 디자인 수정

--- alarm_front/lib/config/colors.dart
+++ alarm_front/lib/config/colors.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
-  static const backgroundColor = Color(0xff121212);
+  static const backgroundColor = Color(0xFF181818);
   static const appbarColor = Color(0xffF5F5F5);
   static const bottomNavColor = Color(0xffE0E0E0);
   static const focusColor = Color(0xff03DAC6);
+  static const sendMsgBurbleColor = Color(0xFF1E90FF);
+  static const receiveMsgBurbleColor = Color(0xFF383838);
+  static const roomListTileColor = Color(0xFF383838);
 }
--- alarm_front/lib/presentation/pages/room_list/room_list_page.dart
+++ alarm_front/lib/presentation/pages/room_list/room_list_page.dart
@@ -1,3 +1,4 @@
+import 'package:alarm_front/config/colors.dart';
 import 'package:flutter/material.dart';
 
 class RoomListPage extends StatelessWidget {
@@ -6,16 +7,198 @@ class RoomListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('RoomListPage'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'RoomListPage is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ChatRoomListPage(),
+    );
+  }
+}
+
+class ChatRoomListPage extends StatelessWidget {
+  // 예시로 채팅방 목록 데이터를 생성
+  final List<ChatRoom> chatRooms = [
+    const ChatRoom(
+        subjectName: 'Flutter 스터디',
+        roomName: '스터디하실 분 모집합니다!',
+        roomStartDate: '2024-08-01 09:24',
+        roomEndDate: '2024-08-24 18:48'),
+    const ChatRoom(
+        subjectName: 'Dart 스터디',
+        roomName: 'dart 공부하실분~',
+        roomStartDate: '2024-08-01 09:24',
+        roomEndDate: '2024-08-24 18:48'),
+    const ChatRoom(
+        subjectName: '배드민턴 치실 분',
+        roomName: '배드민턴 치실 분 구함',
+        roomStartDate: '2024-08-01 09:24',
+        roomEndDate: '2024-08-24 18:48'),
+    const ChatRoom(
+        subjectName: '앱 개발 공부 모입',
+        roomName: '아무나 들어오십쇼',
+        roomStartDate: '2024-08-01 09:24',
+        roomEndDate: '2024-08-24 18:48'),
+    const ChatRoom(
+        subjectName: '테스트 스터디',
+        roomName: '스터디하실 분 모집합니다!',
+        roomStartDate: '2024-08-01 09:24',
+        roomEndDate: '2024-08-24 18:48'),
+    const ChatRoom(
+        subjectName: '테스트 스터디',
+        roomName: '스터디하실 분 모집합니다!',
+        roomStartDate: '2024-08-01 09:24',
+        roomEndDate: '2024-08-24 18:48'),
+    const ChatRoom(
+        subjectName: '테스트 스터디',
+        roomName: '스터디하실 분 모집합니다!',
+        roomStartDate: '2024-08-01 09:24',
+        roomEndDate: '2024-08-24 18:48'),
+    const ChatRoom(
+        subjectName: '프로그래밍 스터디',
+        roomName: '스터디하실 분 모집합니다!',
+        roomStartDate: '2024-08-01 09:24',
+        roomEndDate: '2024-08-24 18:48'),
+    const ChatRoom(
+        subjectName: '프로그래밍 스터디',
+        roomName: '스터디하실 분 모집합니다!',
+        roomStartDate: '2024-08-01 09:24',
+        roomEndDate: '2024-08-24 18:48'),
+    const ChatRoom(
+        subjectName: '프로그래밍 스터디',
+        roomName: '스터디하실 분 모집합니다!',
+        roomStartDate: '2024-08-01 09:24',
+        roomEndDate: '2024-08-24 18:48'),
+  ];
+
+  ChatRoomListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
+      body: ListView.builder(
+        itemCount: chatRooms.length,
+        itemBuilder: (context, index) {
+          return ChatRoomTile(chatRoom: chatRooms[index]);
+        },
       ),
     );
+  }
+}
+
+class ChatRoom extends StatelessWidget {
+  final String subjectName;
+  final String roomName;
+  final String roomStartDate;
+  final String roomEndDate;
+
+  const ChatRoom({
+    super.key,
+    required this.subjectName,
+    required this.roomName,
+    required this.roomStartDate,
+    required this.roomEndDate,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(14.0),
+      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 14.0),
+      decoration: BoxDecoration(
+        color: AppColors.roomListTileColor,
+        borderRadius: BorderRadius.circular(12.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                width: 50,
+                height: 50,
+                child: ClipRect(
+                  child: Image.asset(
+                    '/Users/jhdo/Developer/team2/alarm_front/assets/images/chat_room_default_profile.png',
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(width: 16.0),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '주제: $subjectName',
+                  style: const TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '방 이름: $roomName',
+                  style: const TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 5.0),
+                Text(
+                  '시작: $roomStartDate',
+                  style: const TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.grey,
+                  ),
+                ),
+                Text(
+                  '종료: $roomStartDate',
+                  style: const TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    // fixedSize: const Size(60, 40),
+                    ),
+                onPressed: () {},
+                child: const Text(
+                  '예약',
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ChatRoomTile extends StatelessWidget {
+  final ChatRoom chatRoom;
+
+  const ChatRoomTile({super.key, required this.chatRoom});
+
+  @override
+  Widget build(BuildContext context) {
+    return chatRoom;
   }
 }
