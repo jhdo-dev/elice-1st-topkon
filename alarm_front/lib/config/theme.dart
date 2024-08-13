fe0ed306fccc2e1245f3e9eff5676fab66998a23
[APP1] 도지훈 <zihoon9@gmail.com>
2024-08-13 21:16:28 +0900

    fix: room_create_page 수정

--- alarm_front/lib/config/theme.dart
+++ alarm_front/lib/config/theme.dart
@@ -2,6 +2,7 @@ import 'package:alarm_front/config/colors.dart';
 import 'package:flutter/material.dart';
 
 final ThemeData appTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
   scaffoldBackgroundColor: AppColors.backgroundColor,
   appBarTheme: const AppBarTheme(
     color: AppColors.backgroundColor,
