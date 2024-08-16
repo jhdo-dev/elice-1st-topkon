54d9094ff7c8bb6492961e1fd0db0df0795a9de8
jungseokman <seokman.jung@gmail.com>
2024-08-16 13:18:17 +0900

    방 생성 화면 싱글차일드뷰로 전환

--- alarm_front/lib/presentation/pages/room_create/room_create_page.dart
+++ alarm_front/lib/presentation/pages/room_create/room_create_page.dart
@@ -114,171 +114,174 @@ class _RoomCreatePageState extends State<RoomCreatePage> {
           ),
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.only(top: 50.h, left: 50.w, right: 50.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'ROOM NAME',
-              style: TextStyles.largeTitle,
-            ),
-            const SizedBox(height: 10),
-            Container(
-              height: 50.h,
-              child: TextField(
-                controller: _controller,
-                style: TextStyles.mediumText,
-                decoration: InputDecoration(
-                  hintText: "방 이름을 입력해 주세요.",
-                  hintStyle: TextStyles.mediumText
-                      .copyWith(color: AppColors.hintColor),
-                  border: const OutlineInputBorder(),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: AppColors.bottomNavColor.withOpacity(0.5))),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: AppColors.focusColor.withOpacity(0.5))),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding:
+              EdgeInsets.only(top: 50.h, left: 50.w, right: 50.w, bottom: 50.h),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'ROOM NAME',
+                style: TextStyles.largeTitle,
+              ),
+              const SizedBox(height: 10),
+              Container(
+                height: 50.h,
+                child: TextField(
+                  controller: _controller,
+                  style: TextStyles.mediumText,
+                  decoration: InputDecoration(
+                    hintText: "방 이름을 입력해 주세요.",
+                    hintStyle: TextStyles.mediumText
+                        .copyWith(color: AppColors.hintColor),
+                    border: const OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: AppColors.bottomNavColor.withOpacity(0.5))),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: AppColors.focusColor.withOpacity(0.5))),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 30),
-            Text(
-              'SELECT TOPIC',
-              style: TextStyles.largeTitle,
-            ),
-            const SizedBox(height: 10),
-            Container(
-              width: double.maxFinite,
-              height: 50.h,
-              decoration: BoxDecoration(
-                border: Border.all(
-                    color: AppColors.bottomNavColor.withOpacity(0.5)),
-                borderRadius: BorderRadius.circular(5.h),
+              const SizedBox(height: 30),
+              Text(
+                'SELECT TOPIC',
+                style: TextStyles.largeTitle,
               ),
-              padding: EdgeInsets.symmetric(horizontal: 10.w),
-              child: DropdownButtonHideUnderline(
-                child: BlocBuilder<LoadTopicBloc, TopicState>(
-                  builder: (context, state) {
-                    return DropdownButton<int>(
-                      dropdownColor: AppColors.dropDownColor,
-                      hint: Text(
-                        '주제를 선택해 주세요.',
-                        style: TextStyles.mediumText
-                            .copyWith(color: AppColors.hintColor),
-                      ),
-                      style: TextStyles.mediumText,
-                      isExpanded: true,
-                      value: selectedTopic,
-                      items: state is GetTopicLoaded
-                          ? state.topics
-                              .map(
-                                (e) => DropdownMenuItem<int>(
-                                  child: Text(
-                                    e.name,
-                                    style: TextStyles.mediumText,
+              const SizedBox(height: 10),
+              Container(
+                width: double.maxFinite,
+                height: 50.h,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                      color: AppColors.bottomNavColor.withOpacity(0.5)),
+                  borderRadius: BorderRadius.circular(5.h),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 10.w),
+                child: DropdownButtonHideUnderline(
+                  child: BlocBuilder<LoadTopicBloc, TopicState>(
+                    builder: (context, state) {
+                      return DropdownButton<int>(
+                        dropdownColor: AppColors.dropDownColor,
+                        hint: Text(
+                          '주제를 선택해 주세요.',
+                          style: TextStyles.mediumText
+                              .copyWith(color: AppColors.hintColor),
+                        ),
+                        style: TextStyles.mediumText,
+                        isExpanded: true,
+                        value: selectedTopic,
+                        items: state is GetTopicLoaded
+                            ? state.topics
+                                .map(
+                                  (e) => DropdownMenuItem<int>(
+                                    child: Text(
+                                      e.name,
+                                      style: TextStyles.mediumText,
+                                    ),
+                                    value: e.id,
                                   ),
-                                  value: e.id,
-                                ),
-                              )
-                              .toList()
-                          : [],
-                      onChanged: (int? value) {
-                        if (value != null) {
-                          setState(() {
-                            selectedTopic = value;
-                          });
-                        }
-                      },
-                    );
+                                )
+                                .toList()
+                            : [],
+                        onChanged: (int? value) {
+                          if (value != null) {
+                            setState(() {
+                              selectedTopic = value;
+                            });
+                          }
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Align(
+                alignment: Alignment.center, // 왼쪽 정렬
+                child: Text(
+                  'START DATE',
+                  style: TextStyles.largeTitle,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Center(
+                child: GestureDetector(
+                  onTap: () {
+                    _selectStartDateTime(context);
                   },
-                ),
-              ),
-            ),
-            const SizedBox(height: 30),
-            Align(
-              alignment: Alignment.center, // 왼쪽 정렬
-              child: Text(
-                'START DATE',
-                style: TextStyles.largeTitle,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Center(
-              child: GestureDetector(
-                onTap: () {
-                  _selectStartDateTime(context);
-                },
-                child: Container(
-                  height: 50.h,
-                  decoration: BoxDecoration(
-                    color: AppColors.cardColor,
-                    borderRadius: BorderRadius.circular(5.h),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 3,
-                        color: Colors.black.withOpacity(0.3),
-                        offset: Offset(0, 5),
-                      )
-                    ],
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        selectedStartDateTime != null
-                            ? '${"${selectedStartDateTime!.toLocal()}".split(' ')[0]} ${TimeOfDay.fromDateTime(selectedStartDateTime!).format(context)}'
-                            : 'SELECT',
-                        style: TextStyles.largeText,
-                      ),
-                    ],
+                  child: Container(
+                    height: 50.h,
+                    decoration: BoxDecoration(
+                      color: AppColors.cardColor,
+                      borderRadius: BorderRadius.circular(5.h),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 3,
+                          color: Colors.black.withOpacity(0.3),
+                          offset: Offset(0, 5),
+                        )
+                      ],
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          selectedStartDateTime != null
+                              ? '${"${selectedStartDateTime!.toLocal()}".split(' ')[0]} ${TimeOfDay.fromDateTime(selectedStartDateTime!).format(context)}'
+                              : 'SELECT',
+                          style: TextStyles.largeText,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 30),
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                'END DATE',
-                style: TextStyles.largeTitle,
+              const SizedBox(height: 30),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'END DATE',
+                  style: TextStyles.largeTitle,
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Center(
-              child: GestureDetector(
-                onTap: () {
-                  _selectEndDateTime(context);
-                },
-                child: Container(
-                  height: 50.h,
-                  decoration: BoxDecoration(
-                    color: AppColors.cardColor,
-                    borderRadius: BorderRadius.circular(5.h),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 3,
-                        color: Colors.black.withOpacity(0.3),
-                        offset: Offset(0, 5),
-                      )
-                    ],
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        selectedEndDateTime != null
-                            ? '${"${selectedEndDateTime!.toLocal()}".split(' ')[0]} ${TimeOfDay.fromDateTime(selectedEndDateTime!).format(context)}'
-                            : 'SELECT',
-                        style: TextStyles.largeText,
-                      ),
-                    ],
+              const SizedBox(height: 10),
+              Center(
+                child: GestureDetector(
+                  onTap: () {
+                    _selectEndDateTime(context);
+                  },
+                  child: Container(
+                    height: 50.h,
+                    decoration: BoxDecoration(
+                      color: AppColors.cardColor,
+                      borderRadius: BorderRadius.circular(5.h),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 3,
+                          color: Colors.black.withOpacity(0.3),
+                          offset: Offset(0, 5),
+                        )
+                      ],
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          selectedEndDateTime != null
+                              ? '${"${selectedEndDateTime!.toLocal()}".split(' ')[0]} ${TimeOfDay.fromDateTime(selectedEndDateTime!).format(context)}'
+                              : 'SELECT',
+                          style: TextStyles.largeText,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
