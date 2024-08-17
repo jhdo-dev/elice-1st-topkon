6eb4a2cc3c14f148d011794afefcf42d740ba4c9
jhdodev <jhdo.dev@gmail.com>
2024-08-17 20:18:13 +0900

    feat: 방 생성 시 목록 새로고침 추가

--- alarm_front/lib/presentation/pages/room_create/room_create_page.dart
+++ alarm_front/lib/presentation/pages/room_create/room_create_page.dart
@@ -60,6 +60,9 @@ class _RoomCreatePageState extends State<RoomCreatePage> {
 
               if (state is CreateRoomSuccess) {
                 showCustomSnackbar(context, "방이 생성되었습니다.");
+                context
+                    .read<LoadRoomBloc>()
+                    .add(LoadRoomEvent(topicId: selectedTopic!));
                 context.pop();
               }
             },
