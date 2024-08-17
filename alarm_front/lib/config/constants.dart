cc30531ba842828ef16d43e19564c702ac8dac61
이영인 <zeroinlee@gmail.com>
2024-08-17 19:58:25 +0900

    채팅기능 구현: constants파일 수정

--- alarm_front/lib/config/constants.dart
+++ alarm_front/lib/config/constants.dart
@@ -10,4 +10,14 @@ class Constants {
       return 'http://localhost:3000'; //? 그 외의 경우
     }
   }
+
+  static String get chatUrl {
+    if (Platform.isAndroid) {
+      return 'http://10.0.2.2:3001/chat'; //? Android 에뮬레이터
+    } else if (Platform.isIOS) {
+      return 'http://127.0.0.1:3001/chat'; //? iOS 에뮬레이터
+    } else {
+      return 'http://localhost:3001/chat'; //? 그 외의 경우
+    }
+  }
 }
