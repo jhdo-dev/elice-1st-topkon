02a967227f4eae4f0292ebf3ec4b272238a7e1be
jhdodev <jhdo.dev@gmail.com>
2024-08-19 19:05:57 +0900

    fix: MY Page 방 목록 취소 기능 수정/UX 개선

--- alarm_front/lib/presentation/pages/my/my_page.dart
+++ alarm_front/lib/presentation/pages/my/my_page.dart
@@ -4,7 +4,6 @@ import 'package:alarm_front/presentation/pages/my/widgets/my_info_widget.dart';
 import 'package:alarm_front/utils/snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import '../room_list/widgets/chat_room.dart';
 
 class MyPage extends StatefulWidget {
@@ -21,10 +20,14 @@ class _MyPageState extends State<MyPage> {
     _loadReservedRooms();
   }
 
-  // 비동기 메서드
   Future<void> _loadReservedRooms() async {
     final reservedRooms = await LocalDatasource.getReservedRooms();
-    context.read<LoadRoomsByIdsBloc>().add(LoadRoomsByIdsEvent(reservedRooms));
+    if (mounted) {
+      // 위젯이 여전히 트리에 있는지 확인
+      context
+          .read<LoadRoomsByIdsBloc>()
+          .add(LoadRoomsByIdsEvent(reservedRooms));
+    }
     print('reservedRoom ------> $reservedRooms');
   }
 
@@ -37,8 +40,11 @@ class _MyPageState extends State<MyPage> {
         BlocConsumer<LoadRoomsByIdsBloc, RoomState>(
           listener: (context, state) {
             if (state is GetRoomsByIdsError) {
-              showCustomSnackbar(context, "방 목록을 가져오는데 실패하였습니다.");
-              print('error message ------> ${state.message}');
+              if (context.read<LoadRoomsByIdsBloc>().state
+                  is! GetRoomsByIdsInitial) {
+                showCustomSnackbar(context, "방 목록을 가져오는데 실패하였습니다.");
+                print('error message ------> ${state.message}');
+              }
             }
           },
           builder: (context, state) {
@@ -46,6 +52,20 @@ class _MyPageState extends State<MyPage> {
               return Center(child: CircularProgressIndicator());
             }
             if (state is GetRoomsByIdsLoaded) {
+              if (state.rooms.isEmpty) {
+                return Expanded(
+                  child: Center(
+                    child: Text(
+                      '예약한 방이 없습니다.',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ),
+                );
+              }
               return Expanded(
                 child: ListView.builder(
                   itemCount: state.rooms.length,
