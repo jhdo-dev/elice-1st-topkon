5aa08f85b06915daf30dd1ce9223268bd79331e1
zihoon9 <zihoon9@gmail.com>
2024-08-20 11:35:19 +0900

    Design: 방 목록 램덤프로필 적용/focusColor 변경

--- alarm_front/lib/config/colors.dart
+++ alarm_front/lib/config/colors.dart
@@ -4,7 +4,7 @@ abstract class AppColors {
   static const backgroundColor = Color(0xFF181818);
   static const appbarColor = Color(0xffF5F5F5);
   static const bottomNavColor = Color(0xffE0E0E0);
-  static const focusColor = Color(0xff5bd7ff);
+  static const focusColor = Color(0xff9d73ff);
 
   static const cardColor = Color(0xff1e1e1e);
   static const countBoxColor = Colors.red;
@@ -18,4 +18,20 @@ abstract class AppColors {
 
   static const kakaoColor = Color(0xFFF1DC04);
   static const naverColor = Color(0xFF20C801);
+
+  static const profileRandomColor1 = Color(0xFFf4d3a3);
+  static const profileRandomColor2 = Color(0xFFc3f0b3);
+  static const profileRandomColor3 = Color(0xFFa8d0ed);
+  static const profileRandomColor4 = Color(0xFFd0c7f8);
+  static const profileRandomColor5 = Color(0xFFbfd1e3);
+  static const profileRandomColor6 = Color(0xFFefaeea);
+
+  static const List<Color> profileRandomColors = [
+    profileRandomColor1,
+    profileRandomColor2,
+    profileRandomColor3,
+    profileRandomColor4,
+    profileRandomColor5,
+    profileRandomColor6,
+  ];
 }
