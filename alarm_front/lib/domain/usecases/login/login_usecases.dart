66f8ed5695b1414dfff82c865e9276dd5592b212
zihoon9 <zihoon9@gmail.com>
2024-08-21 16:48:17 +0900

    feature: 카카오 로그인 구현

--- alarm_front/lib/domain/usecases/login/login_usecases.dart
+++ alarm_front/lib/domain/usecases/login/login_usecases.dart
@@ -1,9 +1,12 @@
 import 'package:alarm_front/domain/usecases/login/google_login_usecase.dart';
+import 'package:alarm_front/domain/usecases/login/kakao_login_usecase.dart';
 
 class LoginUseCases {
   final GoogleLoginUsecase googleLoginUsecase;
+  final KakaoLoginUsecase kakaoLoginUsecase;
 
   LoginUseCases({
     required this.googleLoginUsecase,
+    required this.kakaoLoginUsecase,
   });
 }
